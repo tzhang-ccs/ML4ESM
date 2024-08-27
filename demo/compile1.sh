6b85e158f4c039fc39e6d3114e53db98d0403bd5
Tao Zhang <810787812@qq.com>
2024-08-27 12:23:54 -0400

    Update compile1.sh

--- demo/compile1.sh
+++ demo/compile1.sh
@@ -5,6 +5,8 @@ export PYTHONPATH=/global/homes/z/zhangtao/tmp/fortran-call-python/cython/main
 rm main
 rm *.o *.mod
 
+cython ml_pdf_cape.pyx
+
 gcc -c ml_bridge.c ml_pdf_cape.c   -I${include}
 
 gfortran -g -o main ml_wrapper.F90 ml_bridge.o main.f90 ml_pdf_cape.o  -Wl,-rpath ${libs} -L${libs} -lpython3.12
